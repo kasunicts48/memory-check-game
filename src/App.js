1cf3a5df3c1effd6229d71cc1d245e021e5c57de
kasunicts48 <kasundananjayaweerasooriya@gmail.com>
2022-06-06 10:22:57 +0530

    finished shuffling card part

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+
+const cardImages = [
+  {"src": "/img/hero1.png"},
+  {"src": "/img/hero2.png"},
+  {"src": "/img/hero3.png"},
+  {"src": "/img/hero4.png"},
+  {"src": "/img/hero5.png"},
+  {"src": "/img/hero6.png"}
+]
 
 function App() {
-  return (
+
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  // suffle cards
+  const suffleCards = ()=> {
+    const suffledCards = [...cardImages,...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({...card, id : Math.random()}))
+
+    setCards(suffledCards);
+    setTurns(0)
+  }
+
+  console.log(cards, turns);
+
+  return ( 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Memory Hero</h1>
+      <button onClick={suffleCards}>New Game</button>
     </div>
   );
 }
